6346dcf769c7b33f156b7a36b0598a65bdfa138d
Hero Liu <lhard@qq.com>
2017-08-10 15:31:14 +0800

    点击可落子

--- src/index.js
+++ src/index.js
@@ -1,20 +1,60 @@
+/**
+ * Square 组件代表一个单独的 <button>。
+ * Board 组件包含了9个squares，也就是棋盘的9个格子。
+ * Game 组件则为我们即将要编写的代码预留了一些位置。
+ */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
-  }
+// class Square extends React.Component {
+//   // constructor() {
+//   //   // 在使用 JavaScript classes 时，你必须调用 super(); 方法才能在继承父类的子类中正确获取到类型的 this 。
+//   //   super();
+//   //   this.state = {
+//   //     value: null,
+//   //   }
+//   // }
+//   render() {
+//     return (
+//       <button className="square" onClick={() => this.props.onClick()}>
+//         {this.props.value}
+//       </button>
+//     );
+//   }
+// }
+// 若只有render函数，则可以写成如下部分
+function Square(props) {
+  return (
+    <button className="square" onClick={() => props.onClick()}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      squares: Array(9).fill(null),
+    }
+  }
+
+  handleClick(i) {
+    // 常量对于引用类型，只是指向其内存地址，地址所对应的内存内容可以改变。
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares});
+  }
+
   renderSquare(i) {
-    return <Square />;
+    return (
+      <Square 
+        value = {this.state.squares[i]}
+        onClick = {() => this.handleClick(i)}
+      />
+      // 此行上面不能加；号，而下面圆括号后需要加；号。
+    );
   }
 
   render() {
