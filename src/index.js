ae691e66e6554d477e9c2e6479437c8963c38ae0
Hero Liu <lhard@qq.com>
2017-08-11 16:33:52 +0800

    循环遍历获取六宫格布局

--- src/index.js
+++ src/index.js
@@ -37,7 +37,8 @@ class Board extends React.Component {
 
   renderSquare(i) {
     return (
-      <Square 
+      <Square
+        key = {i}
         value = {this.props.squares[i]}
         onClick = {() => this.props.onClick(i)}
       />
@@ -46,23 +47,17 @@ class Board extends React.Component {
   }
 
   render() {
+    let rows = [];
+    for (let i = 0; i < 3; i++) {
+      let row = [];
+      for (let j = 3 * i; j < 3 * i + 3; j++) {
+        row.push(this.renderSquare(j));
+      }
+      rows.push(<div className="board-row" key={i}>{row}</div>);
+    }
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {rows}
       </div>
     );
   }
